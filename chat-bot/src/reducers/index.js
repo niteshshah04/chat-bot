ef00f77d9df8007db35cd3be733be75dac078e1c
Nisaanth Elangovan <se.nisaanth@yahoo.com>
2022-04-17 19:17:19 +0530

    Structure change and date added

--- chat-bot/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-// Import dependencies
-import { combineReducers } from "redux";
-
-// Import reducers
-import chatReducer from "./chatReducer";
-
-// Export combined reducers
-export default combineReducers({ chatReducer });
--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -96,6 +96,7 @@ const Chat = ({ chat, userMessage, sendMessage }) => {
                               <div align="right" className={msg.type}>
                                 <Avatar src="/broken-image.jpg"></Avatar>
                                 {msg.message}
+                                <h6>{new Date().toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' })}</h6>
                               </div>
                             ) : (
                               <div align="left" className={msg.type}>
@@ -103,6 +104,7 @@ const Chat = ({ chat, userMessage, sendMessage }) => {
                                   <AndroidIcon />
                                 </Avatar>
                                 {msg.message}
+                                <h6>{new Date().toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' })}</h6>
                               </div>
                             )
                           )}
